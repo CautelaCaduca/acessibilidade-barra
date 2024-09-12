8edfe59cbabf8412b86a402086a5a990480cc14f
CautelaCaduca <caua.edu.pessoal@gmail.com>
2024-09-12 09:45:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function clicou1(){
+    document.getElementById("imga").style.display = "none"
+    document.getElementById("imgb").style.display = "inline"
+
+    document.getElementById("img1").style.display = "inline"
+    document.getElementById("img2").style.display = "inline"
+    document.getElementById("img3").style.display = "inline"
+    document.getElementById("img4").style.display = "inline"
+
+}
+
+function clicou2(){
+    document.getElementById("imga").style.display = "inline"
+    document.getElementById("imgb").style.display = "none"
+
+    document.getElementById("img1").style.display = "none"
+    document.getElementById("img2").style.display = "none"
+    document.getElementById("img3").style.display = "none"
+    document.getElementById("img4").style.display = "none"
+}
